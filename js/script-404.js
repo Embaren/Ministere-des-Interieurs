1070d3923257779f36b70514110e30ab4421fd1c
Embaren <77256554+Embaren@users.noreply.github.com>
2021-01-12 21:42:58 +0100

    Now with css

--- js/script-404.js
+++ js/script-404.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
 	$("head").append('\
 	<meta charset="utf-8">\
 	<title>404 | Ministère des Intérieurs</title>\
-	<script src="https://code.jquery.com/jquery-3.5.0.js"></script>');
+	<script src="https://code.jquery.com/jquery-3.5.0.js"></script>\
+	<link rel="stylesheet" href="/css/style-404.css">');
 	
 	$("body").empty();
 	$("body").append('\
@@ -12,6 +13,6 @@ $(document).ready(function(){
 		<div id="header-placeholder">\
 		</div>\
 		<script src="/js/script-init.js"></script>\
-		<div>Site en maintenance</div>\
+		<div class="Maintenance">SITE EN MAINTENANCE</div>\
 	</div>');
 });
