5607e5621b85ebb64e9d4794f71a80bd16c6e921
Embaren <77256554+Embaren@users.noreply.github.com>
2021-01-12 20:59:16 +0100

    Update script-init.js

--- js/script-init.js
+++ js/script-init.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$("head").append('\
 	<meta name="description" content="Actualité, clarté, simplicité: retrouvez sur INTERIEURS.RUDF.EU toutes les actions du Ministère des Intérieurs">\
 	<meta name="author" content="Emile Barbier--Renard">\
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">\
 	<link rel="stylesheet" href="/css/style-miccti.css">');
 	
 	$.get("/common/miccti-init.rudf", function (data) {
