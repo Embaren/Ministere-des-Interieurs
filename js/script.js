f1d729bd85c792520b942aed8b3b843492ad88e1
Embaren <77256554+Embaren@users.noreply.github.com>
2021-01-12 01:17:37 +0100

    New version

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$("head").append('\
+	<meta name="description" content="Actualité, clarté, simplicité: retrouvez sur RUDF.eu toutes les actions des RUDF">\
+	<meta name="author" content="Emile Barbier--Renard">\
+	<link rel="stylesheet" href="css/style.css">');
+}); 
