8de9172a84881007bf7f33873b880d769f4f592d
Embaren <77256554+Embaren@users.noreply.github.com>
2021-01-12 20:23:37 +0100

    New version

--- js/script-init.js
+++ js/script-init.js
@@ -2,5 +2,10 @@ $(document).ready(function(){
 	$("head").append('\
 	<meta name="description" content="Actualité, clarté, simplicité: retrouvez sur INTERIEURS.RUDF.EU toutes les actions du Ministère des Intérieurs">\
 	<meta name="author" content="Emile Barbier--Renard">\
-	<link rel="stylesheet" href="css/style.css">');
+	<link rel="stylesheet" href="/css/style-miccti.css">');
+	
+	$.get("/common/miccti-init.rudf", function (data) {
+		$("#header-placeholder").append(data);
+	});
+	
 }); 
