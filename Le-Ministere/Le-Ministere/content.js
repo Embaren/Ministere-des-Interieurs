40696064d3ff7ac6efd771808818f108bafa1d6b
Embaren <77256554+Embaren@users.noreply.github.com>
2021-01-13 21:14:43 +0100

    Update

---
+++ Le-Ministere/Le-Ministere/content.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("head").append('\
+	<title>Le Ministère | Ministère des Intérieurs</title>\
+	
+	$.get("content.rudf", function (data) {
+		$("#maincolumn").append(data);
+	});
+}); 
