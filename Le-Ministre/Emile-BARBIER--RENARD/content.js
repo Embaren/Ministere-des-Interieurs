6ba7c20ddf3fed26b761db32d9910430456aeb5a
Embaren <77256554+Embaren@users.noreply.github.com>
2021-01-14 20:34:47 +0100

    Update

--- Le-Ministre/Emile-BARBIER--RENARD/content.js
+++ Le-Ministre/Emile-BARBIER--RENARD/content.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
-	$("head").append('\
-	<title>Emile BARBIER--RENARD | Ministère des Intérieurs</title>\
+	
+	$.getJSON("children.json", function(json) {
+		
+		$("head").append('\
+	<title>'+json[0]+' | Ministère des Intérieurs</title>\
 	<link rel="stylesheet" href="/css/style-ministre.css">');
+		
+	});
 	
 	$.get("content.rudf", function (data) {
 		$("#maincolumn").append(data);
