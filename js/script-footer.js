c8f202ef2356ca34296c72944d7435fbeeccf091
Embaren <77256554+Embaren@users.noreply.github.com>
2021-01-14 03:44:57 +0100

    Update script-footer.js

--- js/script-footer.js
+++ js/script-footer.js
@@ -1,2 +1,33 @@
 $(document).ready(function(){
+	var title;
+	var htmlbloc;
+	$.getJSON(/children.json", function(json) {
+		json[1].forEach(function(child){
+		htmlbloc='\
+			<div class="bloc-menu">\
+				<h3>\
+					<a href="/'+child.directory+'">'+child.page+'</a>\
+				</h3>\
+				<ul>';
+			
+			$.getJSON("/"+child.directory+"/children.json", function(category) {
+				
+				category[1].forEach(function(page)){
+					htmlbloc=htmlbloc+'\
+					<li>\
+						<a href="/'+child.directory+"/"page.directory+'">'+page.page+'</a>\
+					</li>';
+				});
+				
+			});
+			
+			htmlbloc=htmlbloc+'\
+				</ul>\
+			</div>';
+			
+			$("#footer-site-map").append(htmlbloc);
+
+		});
+	});
+
 }); 
