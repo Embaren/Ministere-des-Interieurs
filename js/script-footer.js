4465bb643fdfa3dd2e9299d19cbb933c741cc8a3
Embaren <77256554+Embaren@users.noreply.github.com>
2021-01-14 22:08:45 +0100

    Update script-footer.js

--- js/script-footer.js
+++ js/script-footer.js
@@ -1,28 +1,34 @@
+function footerConstruct(toExplore) {
+	if(toExplore.length>0){
+		cat=toExplore.pop();
+		$.getJSON("/"+cat.directory+"/children.json", function(category) {
+			htmlbloc=['\
+		<div class="bloc-menu">\
+			<h3>\
+				<a href="/'+cat.directory+'">'+cat.page+'</a>\
+			</h3>\
+			<ul>'];
+			
+			category[1].forEach(function(page){
+				htmlbloc.push('\
+				<li>\
+					<a href="/'+cat.directory+"/"+page.directory+'">'+page.page+'</a>\
+				</li>');
+			});
+			
+			htmlbloc.push('\
+			</ul>\
+		</div>');
+			$("#footer-site-map").append(htmlbloc.join(''));
+			
+			footerConstruct(toExplore);
+		});
+	}
+}
+
 $(document).ready(function(){
 	$.getJSON("/children.json", function(json) {
-		json[1].forEach(function(child){
-			$.getJSON("/"+child.directory+"/children.json", function(category) {
-				htmlbloc=['\
-			<div class="bloc-menu">\
-				<h3>\
-					<a href="/'+child.directory+'">'+child.page+'</a>\
-				</h3>\
-				<ul>'];
-				
-				category[1].forEach(function(page){
-					htmlbloc.push('\
-					<li>\
-						<a href="/'+child.directory+"/"+page.directory+'">'+page.page+'</a>\
-					</li>');
-				});
-				
-				htmlbloc.push('\
-				</ul>\
-			</div>');
-				$("#footer-site-map").append(htmlbloc.join(''));
-			});
-
-		});
+		footerConstruct(json[1]);
 	});
 
 }); 
