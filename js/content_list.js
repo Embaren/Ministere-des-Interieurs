f74c839427ca5a93468cc0ff62cbe1b9c7622075
Embaren <77256554+Embaren@users.noreply.github.com>
2021-01-15 21:24:31 +0100

    Update

--- js/content_list.js
+++ js/content_list.js
@@ -1,3 +1,36 @@
+function newsprevconstruct(Articles,n){
+	if (n!=0 && Articles.length>0){
+		article=Articles.shift();
+		htmlbloc=['\
+		<div class="newsprev">'];
+		if ("picture" in article){
+			htmlbloc.push('\
+			<div class="attribute-image">\
+				<img src="' + article.directory+'/'+article.picture + '" style alt="' + article.page + '" title="' + article.page + '">\
+			</div>');
+		}
+		htmlbloc.push('\
+			<div class="line-content-with-img">');
+		
+		if ("date" in article){
+			htmlbloc.push('\
+				<div class="attribute-date">\
+					'+ article.date +'\
+				</div>');
+		}
+		
+		htmlbloc.push('\
+				<h2>\
+					<a href="' + article.directory + '" title="' + article.page + '">'+ article.page +'</a>\
+				</h2>');
+		htmlbloc.push('\
+			</div>\
+		</div>');
+		$(".content-view-children").append(htmlbloc.join(''));
+		newsprevconstruct(Articles,n-1);
+	}
+}
+
 $(document).ready(function(){
 	$.getJSON("children.json", function(json) {
 		
@@ -5,6 +38,9 @@ $(document).ready(function(){
 	<title>'+json[0]+' | Ministère des Intérieurs</title>');
 		
 		$("#maincolumn").append('\
-	<h1>'+json[0]+'</h1>');
+	<h1>'+json[0]+'</h1>\
+	<div class="content-view-children">\
+	</div>');
 	});
+	newsprevconstruct(json[1],5);
 }); 
